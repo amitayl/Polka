478cc2911ad8708d249192782238e7db69d4f49c
Omri Zentner <omrize@gmail.com>
2018-05-02 22:06:05 +0300

    produc_ page1

--- frontend/src/views/ProductDetails.vue
+++ frontend/src/views/ProductDetails.vue
@@ -2,16 +2,25 @@
 
 
 <template>
-  <div class="container flex space-between product-details">
-     <div class="details ">details</div>
-      <div  class="imgs"> 
-        <div class="flex"> 
-        <img class="primary-img" src="../imgs/car_example2.jpg"> 
-
-         <div class="small-pictures"> details</div>
+  <div class="contain flex space-between product-details">
+       
+       <div  class="product-imgs flex flex-column"> 
+       
+         <img class="primary-img product-img" src="../imgs/car_example2.jpg"> 
+         <div class="small-imgs flex space-between"> 
+             <img class="product-img small-img" src="../imgs/car_example2.jpg">
+             <img class=" product-img small-img" src="../imgs/car_example2.jpg"> 
+             <img class="product-img small-img" src="../imgs/car_example2.jpg"> 
+            <img class="product-img small-img" src="../imgs/car_example2.jpg">  
          </div>
-      </div>
-    </div> 
+         </div>
+         <div class="non-part-imgs  ">
+           <h2 class="product-title title is-2">1940's 2 Toy car</h2>
+           <h4 class="product-desc title is-4" >his toy car was made in 1940's by the Nazi's factories (have certificate to show)</h4> 
+         </div>
+     
+    </div>
+     
       
     
   
@@ -19,14 +28,46 @@
 
 <style scoped>
 
+  div{
+    margin-top:20px;
+  }
   .primary-img{
     width:500px;
     height:300px;
+    
+  }
+  .product-img{
     border: 3px solid rgb(162, 167, 177)
+
   }
   .product-details{
     color: red
   }
+  .non-part-imgs{
+    width:45%;
+  }
+  .product-imgs{
+    width:45%;
+  }
+  .product-title{
+    color:rgb(174, 218, 174)
+  }
+  .product-desc{
+    color:rgb(99, 128, 99)
+  }
+   .product-imgs .small-img{
+     width:20%;
+     height:50px;
+   }
+   .contain {
+    max-width: 1050px;
+    margin: 0 auto;
+    width: 100%;
+    padding-right: 20px;
+    padding-left: 20px;
+}
+
+
 
   
 
