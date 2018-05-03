be754888d304ef37eb7a257c07fc8248b7d83c7c
asaf <asafshpigler@gmail.com>
2018-05-03 10:32:11 +0300

    frontend service function query merge

--- frontend/src/services/ProductService.js
+++ frontend/src/services/ProductService.js
@@ -6,11 +6,6 @@ const BASE_URL = 'http://127.0.0.1:3000';
 // console.log(products);
 
 function query(criteria) {
-<<<<<<< HEAD
-    console.log ('criteria' , criteria );
-    
-    return axios.get(BASE_URL+'/product').then(res => res.data);
-=======
   return axios
     .get(BASE_URL + '/product', {
       params: {
@@ -18,7 +13,6 @@ function query(criteria) {
       }
     })
     .then(res => res.data);
->>>>>>> 86a13404f7eabb9f5a1257d3a5401489c8bd44cc
 }
 
 export default {
